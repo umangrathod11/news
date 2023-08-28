33d3f21dbb71da5561676e81442f4393c5c9ca7c
Umang Rathod <137269426+umangrathod11@users.noreply.github.com>
2023-08-28 18:00:50 +0530

    add a css file

--- src/NewsItem.jsx
+++ src/NewsItem.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './NewsItem.css'; // Import your CSS file
 
 export class NewsItem extends Component {
     render() {
@@ -6,24 +7,24 @@ export class NewsItem extends Component {
 
         return (
             <div className='my-3'>
-                <div className="card">
+                <div className="card App-card"> {/* Add App-card class */}
                     <img
                         src={!imageUrl ? "https://demofree.sirv.com/nope-not-here.jpg" : imageUrl}
-                        className="card-img-top"
+                        className="card-img-top App-image" // Add App-image class
                         alt={content}
                         onError={({ currentTarget }) => {
                             currentTarget.onerror = null;
                             currentTarget.src = "https://demofree.sirv.com/nope-not-here.jpg";
                         }}
                     />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description} . . .</p>
+                    <div className="card-body App-card-body"> {/* Add App-card-body class */}
+                        <h5 className="card-title App-title">{title}</h5> {/* Add App-title class */}
+                        <p className="card-text App-description">{description} . . .</p> {/* Add App-description class */}
                         <a
                             href={newsUrl}
                             target='_blank'
                             rel='noopener noreferrer'
-                            className="btn btn-sm btn-primary"
+                            className="btn btn-sm btn-primary App-link" // Add App-link class
                         >
                             Read more
                         </a>
