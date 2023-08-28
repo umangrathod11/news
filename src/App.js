ebf7a2fa81023f1050fff5f904e64a6bff779c1f
Umang Rathod <137269426+umangrathod11@users.noreply.github.com>
2023-08-28 17:46:30 +0530

    add new

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
-import React, { useState, useEffect } from 'react';
 import './App.css';
-import NewsCard from './NewsCard';
-
-const API_KEY = 'YOUR_API_KEY'; // Replace with your actual API key
-const API_URL = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
-
-const App = () => {
-  const [articles, setArticles] = useState([]);
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then(response => response.json())
-      .then(data => setArticles(data.articles))
-      .catch(error => console.error('Error fetching news:', error));
-  }, []);
-
-  return (
-    <div className="app">
-      <header>
-        <h1>News API Website</h1>
-      </header>
-      <main className="news-container">
-        {articles.map(article => (
-          <NewsCard key={article.title} article={article} />
-        ))}
-      </main>
-    </div>
-  );
-};
-
-export default App;
+import React, { Component } from 'react'
+import News from './News';
+export default class App extends Component {
+  pageSize = 9;
+  render() {
+    return (
+      <div>
+        <News/>
+     
+      </div>
+    )
+  }
+}
---
+++ src/NewsItem.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export class NewsItem extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl, content } = this.props;
+
+        return (
+            <div className='my-3'>
+                <div className="card">
+                    <img
+                        src={!imageUrl ? "https://demofree.sirv.com/nope-not-here.jpg" : imageUrl}
+                        className="card-img-top"
+                        alt={content}
+                        onError={({ currentTarget }) => {
+                            currentTarget.onerror = null;
+                            currentTarget.src = "https://demofree.sirv.com/nope-not-here.jpg";
+                        }}
+                    />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description} . . .</p>
+                        <a
+                            href={newsUrl}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            className="btn btn-sm btn-primary"
+                        >
+                            Read more
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default NewsItem;
